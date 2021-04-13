af446dda962d7c5d3e0313704a2942246c9916ec
sy <somarl@live.com>
2021-04-14 00:03:25 +0800

    add global context and useContext hook

--- src/pages/me/index.js
+++ src/pages/me/index.js
@@ -1,30 +1,42 @@
 
 import * as React from 'react'
-import {useState} from 'react'
+import {useEffect} from 'react'
 import {View, Image} from 'remax/wechat'
 
 import LoginButton from '../../components/LoginButton'
+import {useContext} from '../../hooks'
 
 import './index.css'
 
 
 const IndexPage = () => {
-  const [avatar, setAvatar] = useState('')
+  const [context, setContext] = useContext()
 
   const onGetUserInfo = event => {
-    console.log('avatarUrl', event.userInfo.avatarUrl)
+    console.log('avatarUrl', event.userInfo.avatarUrl) // TODELETE
     const {userInfo} = event
 
-    setAvatar(userInfo.avatarUrl)
+    setContext(prev => ({
+      ...prev,
+      user: {
+        ...prev.user,
+        avatarUrl: userInfo.avatarUrl,
+      }
+    }))
   }
+
+  useEffect(() => {
+    console.log('context', context)
+    console.count('rerender me')
+  }, [context])
 
   return (
     <View className="app">
       <View className="header">
-        {avatar
+        {context.user.avatarUrl
           ? (
             <Image
-              src={avatar}
+              src={context.user.avatarUrl}
               className="logo"
               alt="logo"
             />
