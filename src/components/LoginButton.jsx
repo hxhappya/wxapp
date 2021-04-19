76575a30dd0b565a08af01d8468e6acd5831d047
sy <somarl@live.com>
2021-04-19 22:24:14 +0800

    feat(component): refactor login button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,14 +2,49 @@
 import * as React from 'react'
 import {Button} from 'remax/wechat'
 
+import {useContext} from '../hooks'
+
+
+async function getUserInfo (data) {
+  // return wx.cloud.callFunction({
+  //   name: 'getUser',
+  //   data,
+  // })
+  return {
+    userId: 1,
+    avatarUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIJR7AePS7nJbCWzFuiaqgRRic1Ughu5fIFDVRHDLsDeo5R1godU5bdbmp338UOg5FVo9E0iamFsbh0Q/132',
+    nickName: 'Sy',
+    credit: Math.random() * 1000 | 0,
+  }
+}
+
 
 const LoginButton = ({onGetUserInfo, children}) => {
-  const onClick = event => {
-    wx.getUserProfile({
-      desc: '用于完善会员资料',
-      success: onGetUserInfo,
+  const [, setContext] = useContext()
+
+  const saveUserInfo = userInfo => {
+    setContext(prev => ({
+      ...prev,
+      user: userInfo
+    }))
+    wx.setStorage({
+      key: 'userInfo',
+      data: userInfo,
     })
+    if (onGetUserInfo) onGetUserInfo(userInfo)
   }
+
+  const onClick = async event => {
+    const storage = wx.getStorageSync('userInfo')
+    const cloudResult = await getUserInfo(storage && storage.userId
+      ? storage
+      : await wx.getUserProfile({
+        desc: '用于完善会员资料',
+      })
+    )
+    saveUserInfo(cloudResult)
+  }
+
   return (
     <Button
       className="login-button"
